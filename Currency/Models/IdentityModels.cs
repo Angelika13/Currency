d85daa4bb92bcac28aa964798ccb615e64a7f4f2
Angelika13 <62874960+Angelika13@users.noreply.github.com>
2020-08-18 12:57:16 +0300

    2

---
+++ Currency/Models/IdentityModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency.Models
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext() : base("TESTBASE") { }
+        public DbSet<Valuta> Valutas { get; set; }
+        public DbSet<ValCurs> ValCurs { get; set; }
+
+        public static EFDbContext Create()
+        {
+            return new EFDbContext();
+        }
+    }
+}
---
+++ Currency/Models/Models.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Currency.Models
+{
+    public class Valuta
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string EngName { get; set; }
+        public int Nominal { get; set; }
+        public int ISO_Num_Code { get; set; }
+        public string ISO_Char_Code { get; set; }
+
+    }
+    public class ValCurs
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        public DateTime CursDate { get; set; }
+        public int ValutaId { get; set; }
+        public Valuta Valuta { get; set; }
+    }
+    public class CursShow
+    {
+        public decimal Curs { get; set; }
+        public string Str { get; set; }
+    }
+}
